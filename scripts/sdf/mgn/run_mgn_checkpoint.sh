3885b343591474e5d6dd2203a6cf437ebe9a5468
Tiffany Fan <tiffan@stanford.edu>
2024-11-11 12:25:46 -0800

    Added simulation scripts

--- scripts/sdf/mgn/run_mgn_checkpoint.sh
+++ scripts/sdf/mgn/run_mgn_checkpoint.sh
@@ -9,9 +9,9 @@
 #SBATCH --gpus=1
 #SBATCH --nodes=1
 #SBATCH --mem-per-cpu=16G
-#SBATCH --time=10:00:00
+#SBATCH --time=12:00:00
 # =============================================================================
-# SLURM Job Configuration for Mesh Graph AutoEncoder (mgn-ae)
+# SLURM Job Configuration for Mesh Graph Net (mgn)
 # =============================================================================
 
 # Bind CPUs to cores for optimal performance
@@ -51,10 +51,22 @@ TASK="predict_n6d"             # Replace with your specific task
 MODE="train"
 NTRAIN=4156
 BATCH_SIZE=32
-NEPOCHS=1000
+NEPOCHS=2000
 HIDDEN_DIM=256
 NUM_LAYERS=6                   # Must be even for autoencoders (encoder + decoder)
-CHECKPOINT="/sdf/data/ad/ard/u/tiffan/results/mgn/graph_data_filtered_total_charge_51/predict_n6d/knn_k5_weighted_r63_nt4156_b32_lr0.0001_h256_ly6_pr1.00_ep1000/checkpoints/model-419.pth"
+
+# Learning rate scheduler parameters
+LR=1e-4
+LR_SCHEDULER="lin"
+LIN_START_EPOCH=100
+LIN_END_EPOCH=1000
+LIN_FINAL_LR=1e-5
+
+# Random seed
+RANDOM_SEED=63
+
+# Checkpoint path
+CHECKPOINT="/sdf/data/ad/ard/u/tiffan/results/mgn/graph_data_filtered_total_charge_51/predict_n6d/knn_k5_weighted_r63_nt4156_b32_lr0.0001_h256_ly6_pr1.00_ep1000/checkpoints/model-999.pth"
 
 # =============================================================================
 # Construct the Python Command with All Required Arguments
@@ -73,6 +85,12 @@ python_command="python src/graph_models/train.py \
     --nepochs $NEPOCHS \
     --hidden_dim $HIDDEN_DIM \
     --num_layers $NUM_LAYERS
+    --lr $LR \
+    --lr_scheduler $LR_SCHEDULER \
+    --lin_start_epoch $LIN_START_EPOCH \
+    --lin_end_epoch $LIN_END_EPOCH \
+    --lin_final_lr $LIN_FINAL_LR \
+    --random_seed $RANDOM_SEED \
     --checkpoint $CHECKPOINT"
 
 # =============================================================================
--- src/simulations/SLURM2_Low_FIdelity_Gaussian_Rotate_Constant_Settings_4.sh
+++ src/simulations/SLURM2_Low_FIdelity_Gaussian_Rotate_Constant_Settings_4.sh
@@ -4,12 +4,12 @@
 #SBATCH --job-name=xopt-impact
 #SBATCH --output=job_%j.out
 #SBATCH --error=job_%j.err
-#SBATCH --ntasks=1
+#SBATCH --ntasks=240
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4g
 #SBATCH --time=100:30:00
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 
 export PMIX_MCA_psec=^munge
 
-/usr/lib64/openmpi/bin/mpirun python -m mpi4py.futures -m xopt.mpi.run xopt2_Low_Fidelity_Gaussian_Rotate_Constant_Settings_1mm.yaml
+/usr/lib64/openmpi/bin/mpirun python -m mpi4py.futures -m xopt.mpi.run xopt2_Low_Fidelity_Gaussian_Rotate_Constant_Settings_4.yaml
