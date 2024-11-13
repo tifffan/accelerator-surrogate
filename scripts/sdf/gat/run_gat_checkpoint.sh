9b81dd81fe55f960c78b27ac3dd0359fcaef791f
Tiffany Fan <tiffan@stanford.edu>
2024-11-12 19:13:52 -0800

    Edited training script

--- scripts/sdf/gat/run_gat_checkpoint.sh
+++ scripts/sdf/gat/run_gat_checkpoint.sh
@@ -44,8 +44,9 @@ LIN_END_EPOCH=1000
 LIN_FINAL_LR=1e-5
 GAT_HEADS=4
 
-CHECKPOINT=None
-# CHECKPOINT="/sdf/data/ad/ard/u/tiffan/results/gat/graph_data_filtered_total_charge_51/predict_n6d/knn_k5_weighted_r63_nt4156_b32_lr0.0001_h256_ly6_pr1.00_ep2000_sch_lin_100_1000_1e-05/checkpoints/model-1909.pth"
+# CHECKPOINT=None
+CHECKPOINT="/sdf/data/ad/ard/u/tiffan/results/gat/graph_data_filtered_total_charge_51/predict_n6d/knn_k5_weighted_r63_nt4156_b32_lr0.0001_h256_ly6_pr1.00_ep2000_sch_lin_100_1000_1e-05_heads4/checkpoints/model-659.pth"
+
 RANDOM_SEED=63
 
 python_command="python src/graph_models/train.py \
